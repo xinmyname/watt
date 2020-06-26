ca975f52883fdafe19cabc256349cf5eb7b3bb55
Andy Sherwood <xinmyname@gmal.com>
2020-06-26 07:13:55 -0700

    enum for command line args

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,48 @@
+use std::env;
 use std::error::Error;
+use std::fmt;
+use std::collections::VecDeque;
+
+pub enum Command {
+    Unknown,
+    Init,
+    Generate
+}
+
+impl fmt::Display for Command {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Command::Unknown => write!(f, "Unknown"),
+            Command::Init => write!(f, "Init"),
+            Command::Generate => write!(f, "Generate")
+        }
+    }
+}
 
 pub struct Config {
-    pub command: String,
+    pub command: Command,
 }
 
 impl Config {
 
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
 
-        if args.len() < 3 {
+    pub fn new(env_args: env::Args) -> Result<Config, &'static str> {
+
+        let mut args: VecDeque<String> = VecDeque::new();
+
+        for arg in env_args.skip(1) {
+            args.push_back(arg);
+        }
+
+        if args.len() < 1 {
             return Err("not enough arguments");
         }
 
-        let command = args[1].clone();
+        let command_arg = args.pop_front().unwrap();
+
+        println!("command arg: {}", command_arg);
+
+        let command = Command::Unknown;
 
         Ok(Config {
             command
